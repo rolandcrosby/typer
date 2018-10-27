e6be0e97609ac0b6095a47cdad5168a313513d50
Roland Crosby <roland@cockroachlabs.com>
2018-10-27 13:32:57 -0400

    add queueing of multiple actions

--- static/client.js
+++ static/client.js
@@ -1,32 +1,106 @@
-const defaultText = `$ mysqldump drupal > drupal.sql
-Enter password:
-$ cockroach sql --set display_format=records -e "CREATE DATABASE test; USE test; IMPORT MYSQLDUMP ('nodelocal:///drupal.sql');"
--[ RECORD 1 ]
-job_id             | 394965594449870849
-status             | succeeded
-fraction_completed | 1
-rows               | 6789
-index_entries      | 7453
-system_records     | 0
-bytes              | 6679075
-$ `;
+window.onload = function() {
+  const w = 120;
+  const h = 16;
+  const buffer = {
+    width: w,
+    height: h,
+    chars: new Array(w * h),
+    pos: 0
+  };
+  buffer.chars.fill("");
+  render(buffer);
+  function getNext() {
+    if (demoIO.length == 0) {
+      return () => {};
+    }
+    const instruction = demoIO.shift(0);
+    let ret;
+    switch (instruction.type) {
+      case "out":
+        ret = () => writeOutput(buffer, instruction.text, getNext);
+        break;
+      case "in":
+        ret = () => writeInput(buffer, instruction.text, getNext);
+        break;
+      case "pause":
+        ret = () => pause(instruction.length, getNext);
+        break;
+      case "password":
+        ret = () => writePassword(instruction.length, getNext);
+        break;
+    }
+    return ret;
+  }
+  getNext()();
+};
 
-const w = 120;
-const h = 16;
+function render(dataset) {
+  var characters = d3
+    .select(".terminal__frame")
+    .selectAll("div")
+    .data(dataset.chars);
 
-function parse(input) {
-  const out = new Array(w * h);
-  out.fill("");
-  var pos = 0;
-  for (var i = 0; i < input.length; i++) {
-    if (input[i] == "\n") {
-      pos = (Math.floor(pos / w) + 1) * w;
+  characters
+    .enter()
+    .append("div")
+    .merge(characters)
+    .text(d => d)
+    .classed("terminal__cursor", (d, i) => i == dataset.pos);
+
+  characters.exit().remove();
+}
+
+function writeOutput(buffer, text, next) {
+  for (var i = 0; i < text.length; i++) {
+    let char = text[i];
+    if (char == "\n") {
+      buffer.pos = (Math.floor(buffer.pos / buffer.width) + 1) * buffer.width;
     } else {
-      out[pos] = input[i];
-      pos++;
+      buffer.chars[buffer.pos] = char;
+      buffer.pos++;
     }
   }
-  return out;
+  render(buffer);
+  setTimeout(next(), 0);
+}
+
+function writeInput(buffer, text, next) {
+  if (text.length == 0) {
+    setTimeout(next(), 0);
+    return;
+  }
+  let soundName = "key";
+  const char = text[0];
+  if (char == "\n") {
+    buffer.pos = (Math.floor(buffer.pos / buffer.width) + 1) * buffer.width;
+    soundName = "delete";
+  } else {
+    buffer.chars[buffer.pos] = char;
+    buffer.pos++;
+    if (char == " ") {
+      soundName = "space";
+    }
+  }
+  playSound(soundName);
+  render(buffer);
+  setTimeout(() => {
+    writeInput(buffer, text.substr(1), next);
+  }, 40 + Math.random() * 80);
+}
+
+function writePassword(length, next) {
+  if (length < 1) {
+    setTimeout(next(), 0);
+    return;
+  }
+  playSound("key");
+  setTimeout(() => {
+    writePassword(length - 1, next);
+  }, 40 + Math.random() * 80);
+}
+
+function pause(length, next) {
+  setTimeout(next(), length);
 }
 
 const soundPool = {};
@@ -44,36 +118,32 @@ function playSound(identifier) {
   }
 }
 
-window.onload = function() {
-  var chars = 1;
-  const draw = function() {
-    update(parse(defaultText.substr(0, chars)));
-    if (defaultText[chars - 1] == " ") {
-      playSound("space");
-    } else if (defaultText[chars - 1] == "\n") {
-      playSound("delete");
-    } else {
-      playSound("key");
-    }
-    if (chars < defaultText.length) {
-      chars++;
-      setTimeout(draw, 40 + Math.random() * 80);
-    }
-  };
-  draw();
-};
-
-function update(dataset) {
-  var characters = d3
-    .select(".terminal__frame")
-    .selectAll("div")
-    .data(dataset);
-
-  characters
-    .enter()
-    .append("div")
-    .merge(characters)
-    .text(d => d);
-
-  characters.exit().remove();
-}
+const demoIO = [
+  { type: "out", text: "$ " },
+  { type: "pause", length: 600 },
+  { type: "in", text: "mysqldump drupal > drupal.sql\n" },
+  { type: "out", text: "Enter password: " },
+  { type: "pause", length: 600 },
+  { type: "password", length: 8 },
+  { type: "in", text: "\n" },
+  { type: "pause", length: 2000 },
+  { type: "out", text: "$ " },
+  { type: "pause", length: 1000 },
+  {
+    type: "in",
+    text: `cockroach sql --set display_format=records -e "CREATE DATABASE test; USE test; IMPORT MYSQLDUMP ('nodelocal:///drupal.sql');"\n`
+  },
+  { type: "pause", length: 2400 },
+  {
+    type: "out",
+    text: `-[ RECORD 1 ]
+job_id             | 394965594449870849
+status             | succeeded
+fraction_completed | 1
+rows               | 6789
+index_entries      | 7453
+system_records     | 0
+bytes              | 6679075
+$ `
+  }
+];
